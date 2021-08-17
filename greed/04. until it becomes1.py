d11428f95cc96e83887c3de6d6f719fa9403dd54
heejung-gjt <hee_9803@naver.com>
2021-08-17 09:35:36 +0900

    0817 solve  3 question

--- greed/04. until it becomes1.py
+++ greed/04. until it becomes1.py
@@ -18,4 +18,26 @@ while True:
         cnt += 1
         n //= k
 
-# 범위가 클 경우 
+# 범위가 클 경우 (시간복잡도 : log)
+
+n, k = map(int, input().split())
+result = 0
+
+while True:
+    target = (n // k) * k # n을 k로 나눌때 나누어떨어지지 않는 경우 k로 나누어 떨어지는 가장 가까운 수 n이 나오게 됨
+    result += (n - target) # 1을 빼는 연산의 개수를 한번에 해준다.
+    n = target
+    if n < k:
+        break
+    result += 1
+    n //= k
+
+result += (n - 1)
+
+
+
+
+
+
+
+
